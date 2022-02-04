f40f0942aecffafee957b50d1df9950aa2b8ced4
maliut <lqn619@163.com>
2022-02-04 23:19:45 +0800

    feat: vite 多页面配置

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        admin: resolve(__dirname, 'admin/index.html')
+      }
+    }
+  }
+})
