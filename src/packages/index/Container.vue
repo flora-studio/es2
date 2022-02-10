602e5a48e98d8d36c6fb08ecc44ac823184b6734
maliut <lqn619@163.com>
2022-02-11 00:23:53 +0800

    feat: 抽卡结果展示

--- src/packages/index/Container.vue
+++ src/packages/index/Container.vue
@@ -3,22 +3,26 @@
     <thumb-scouts />
     <gacha-button @click="clickTake1" />
     <div style="position: absolute; right: 0; top: 0">水位：{{ waterLevel }}</div>
+    <result-view v-if="gachaResult !== null" v-model:value="gachaResult" />
   </div>
 </template>
 <script setup lang="ts">
 import ThumbScouts from './ThumbScouts.vue'
 import {ref} from 'vue'
 import GachaButton from './GachaButton.vue'
-import {Scout} from '../../composables/useScouts'
 import {waterLevel} from './logic/store'
 import {take1, take10} from './logic/actions'
+import ResultView from './ResultView.vue'
+import {Card} from '../../composables/useCards'
 
 // 当前选择的池子
 // const selectPool = ref<Scout | null>(null)
+const gachaResult = ref<Card[] | null>(null)
 
 const clickTake1 = () => {
   const result = take10()
   console.table(result)
+  gachaResult.value = result
 }
 
 </script>
---
+++ src/packages/index/ResultView.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="result-view">
+    <div class="grid">
+      <div v-for="card in value" :key="card.id">
+        <!-- todo 加载中样式 -->
+        <img :src="card.img" :alt="card.name" :title="card.name" referrerpolicy="no-referrer" />
+      </div>
+    </div>
+    <div class="actions">
+      <div @click="$emit('update:value', null)">OK</div>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+import {Card} from '../../composables/useCards'
+
+const props = defineProps<{ value: Card[] | null }>()
+const emit = defineEmits<{
+  (e: 'update:value', value: Card[] | null): void
+}>()
+</script>
+<style scoped>
+.result-view {
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  padding: 1% 5%;
+  box-sizing: border-box;
+  background: rgba(255, 255, 255, 0.8);
+}
+
+.grid {
+  height: 80%;
+  display: grid;
+  grid-template-columns: repeat(5, 20%);
+  grid-template-rows: repeat(2, 50%);
+}
+
+.grid > div {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+img {
+  display: block;
+  width: 90%;
+  background-color: darkgrey;
+}
+</style>
