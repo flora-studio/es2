3a2582d3e5a1e8a3185cf71c1befec7220cf6ea7
maliut <lqn619@163.com>
2022-02-13 17:22:34 +0800

    fix: 抽卡逻辑

--- src/packages/index/logic/actions.ts
+++ src/packages/index/logic/actions.ts
@@ -1,6 +1,6 @@
 import {Card} from '../../../composables/useCards'
 import {cardRange, cardsCounter, currentScout, Gacha, history, upCardsIds, waterLevel} from './store'
-import {findLastIndex, shuffle} from 'lodash-es'
+import {shuffle} from 'lodash-es'
 
 type ResultType = { up: boolean, star: number }
 
@@ -8,7 +8,8 @@ type ResultType = { up: boolean, star: number }
 export function take1() {
   console.assert(currentScout.value !== null, 'must choose a scout')
   const scout = currentScout.value!
-  const resultType = (waterLevel.value === 49 ? randomResultType_Least5 : randomResultType_General)(scout.type === 'normal')
+  const gachaMethod = waterLevel.value === 49 ? randomResultType_Exact5 : randomResultType_General
+  const resultType = gachaMethod(scout.type === 'normal')
   const result = randomCard(cardRangeByResultType(resultType))
   // 历史记录
   recordHistory({ type: scout.type, series: scout.series, cardIds: [result.id] })
@@ -25,35 +26,32 @@ export function take1() {
 export function take10() {
   console.assert(currentScout.value !== null, 'must choose a scout')
   const scout = currentScout.value!
-  const result: Card[] = []
+  const batchResult: Card[] = []
   const isNormal = scout.type === 'normal'
   logD('【十连】当前水位', waterLevel.value)
-  // 计算 50 抽保底
-  if (waterLevel.value >= 40) {
-    logD('水位达到50，触发保底五星')
-    const least5Result = randomCard(cardRangeByResultType(randomResultType_Least5(isNormal)))
-    result.push(least5Result)
+  // 对十连抽的每个位置进行抽取
+  for (let i = 0; i < 10; i++) {
+    const gachaMethod = waterLevel.value === 49 ? randomResultType_Exact5 : randomResultType_General
+    const resultType = gachaMethod(scout.type === 'normal')
+    const result = randomCard(cardRangeByResultType(resultType))
+    batchResult.push(result)
+    // 水位处理
+    if (result.star === 5) {
+      waterLevel.value = 0
+    } else {
+      waterLevel.value++
+    }
   }
-  // 计算 10 连保底
-  logD('触发保底四星')
-  const least4Result = randomCard(cardRangeByResultType(randomResultType_Least4(isNormal)))
-  result.push(least4Result)
-  // 抽取剩余
-  const restLength = 10 - result.length
-  for (let i = 0; i < restLength; i++) {
-    result.push(randomCard(cardRangeByResultType(randomResultType_General(isNormal))))
+  // 塞个保底四星
+  const giftPos = Math.floor(Math.random() * 10)
+  logD('保底四星 位置', giftPos)
+  if (batchResult[giftPos].star < 4) {
+    batchResult[giftPos] = randomCard(cardRangeByResultType(randomResultType_Exact4(isNormal)))
   }
-  // 打乱
-  const shuffled = shuffle(result)
+  // 打乱并展示
+  const shuffled = shuffle(batchResult)
   // 历史记录
   recordHistory({ type: scout.type, series: scout.series, cardIds: shuffled.map(card => card.id) })
-  // 水位处理
-  const lastStar5Pos = findLastIndex(shuffled, card => card.star === 5)
-  if (lastStar5Pos >= 0) {
-    waterLevel.value = 9 - lastStar5Pos
-  } else {
-    waterLevel.value += 10
-  }
   return shuffled
 }
 
@@ -84,14 +82,10 @@ function randomResultType_General(isNormal = false): ResultType {
 }
 
 // 计算保底 4 星概率
-function randomResultType_Least4(isNormal = false): ResultType {
-  const r = Math.floor(Math.random() * 100)
+function randomResultType_Exact4(isNormal = false): ResultType {
+  const r = Math.floor(Math.random() * 7)
   const ret = (() => {
-    if (r === 0) {
-      return { up: true, star: 5 }
-    } else if (r > 0 && r <= 2) {
-      return { up: false, star: 5 }
-    } else if (r > 2 && r <= 30) {
+    if (r < 2) {
       return { up: true, star: 4 }
     } else {
       return { up: false, star: 4 }
@@ -106,7 +100,7 @@ function randomResultType_Least4(isNormal = false): ResultType {
 }
 
 // 计算保底 5 星概率
-function randomResultType_Least5(isNormal = false): ResultType {
+function randomResultType_Exact5(isNormal = false): ResultType {
   const r = Math.floor(Math.random() * 3)
   const ret = (() => {
     if (r === 0) {
