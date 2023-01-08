0dd439fbde32ad282705af2e9ebb9a62fe9c8bec
maliut <lqn619@163.com>
2023-01-08 16:19:29 +0800

    feat: 更新卡池 67

--- crawler/updater/index.js
+++ crawler/updater/index.js
@@ -3,8 +3,8 @@ const fs = require('fs')
 const BASE_URL = 'https://ensemble-stars.fandom.com'
 
 // TODO 每次记得替换
-const ScoutUrl = 'https://ensemble-stars.fandom.com/wiki/Category:Arashi_Feature_Scout_2'
-const ScoutSeries = 65
+const ScoutUrl = 'https://ensemble-stars.fandom.com/wiki/Category:Mika_Feature_Scout_2'
+const ScoutSeries = 67
 
 const scout = {
   type: '',
