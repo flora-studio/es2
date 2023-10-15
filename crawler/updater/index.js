30ed4864ef26e151ba95ae56c8040af9a0ee9d14
maliut <lqn619@163.com>
2023-10-15 16:12:07 +0800

    feat: 更新卡池 85

--- crawler/updater/index.js
+++ crawler/updater/index.js
@@ -3,8 +3,8 @@ const fs = require('fs')
 const BASE_URL = 'https://ensemble-stars.fandom.com'
 
 // TODO 每次记得替换
-const ScoutUrl = 'https://ensemble-stars.fandom.com/wiki/Category:Jun_Feature_Scout_2'
-const ScoutSeries = 84
+const ScoutUrl = 'https://ensemble-stars.fandom.com/wiki/Category:Subaru_Feature_Scout_2'
+const ScoutSeries = 85
 
 const scout = {
   type: '',
