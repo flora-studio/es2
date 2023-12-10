00a7e519f202f5f85ff230840b432f04f41edb7c
maliut <lqn619@163.com>
2023-12-10 15:14:54 +0800

    feat: 更新卡池 89

--- crawler/updater/index.js
+++ crawler/updater/index.js
@@ -3,8 +3,8 @@ const fs = require('fs')
 const BASE_URL = 'https://ensemble-stars.fandom.com'
 
 // TODO 每次记得替换
-const ScoutUrl = 'https://ensemble-stars.fandom.com/wiki/Category:Chiaki_Feature_Scout_2'
-const ScoutSeries = 88
+const ScoutUrl = 'https://ensemble-stars.fandom.com/wiki/Category:Color_Code'
+const ScoutSeries = 89
 
 const scout = {
   type: '',
