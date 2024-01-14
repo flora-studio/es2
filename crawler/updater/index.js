9ec97b7b5418d975a278c698c87a89dc2c934435
maliut <lqn619@163.com>
2024-01-14 15:30:26 +0800

    feat: 更新卡池 91

--- crawler/updater/index.js
+++ crawler/updater/index.js
@@ -3,8 +3,8 @@ const fs = require('fs')
 const BASE_URL = 'https://ensemble-stars.fandom.com'
 
 // TODO 每次记得替换
-const ScoutUrl = 'https://ensemble-stars.fandom.com/wiki/Category:Kanata_Feature_Scout'
-const ScoutSeries = 40
+const ScoutUrl = 'https://ensemble-stars.fandom.com/wiki/Category:Kaoru_Feature_Scout_2'
+const ScoutSeries = 91
 
 const scout = {
   type: '',
