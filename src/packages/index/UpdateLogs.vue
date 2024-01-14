ad17015023a46c71fc727283c50723486e2a24d1
maliut <lqn619@163.com>
2024-01-14 15:42:22 +0800

    fix: images

--- src/packages/index/UpdateLogs.vue
+++ src/packages/index/UpdateLogs.vue
@@ -4,9 +4,9 @@
     <div class="inner">
       <a href="#" /><!-- 处理焦点的问题 -->
       <h3>最近更新</h3>
+      <p>2024/01/14 更新日服最新池</p>
+      <p>2023/12/24 更新日服最新池</p>
       <p>2023/12/10 更新日服最新池</p>
-      <p>2023/11/26 更新日服最新池</p>
-      <p>2023/11/12 更新日服最新池</p>
       <h3>关于我们</h3>
       <p>FloraStudio 专注于游戏、二次元等领域的创作与分析。欢迎关注我们：</p>
       <p>Bilibili: <a href="https://space.bilibili.com/688429881" target="_blank" referrerpolicy="no-referrer">@FloraStudio</a>
